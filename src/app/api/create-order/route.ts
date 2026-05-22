d63ca9070a6d1dc76edc3256cc151942c0128a8a
Priyanka Sharma <ps0665818@gmail.com>
2026-05-22 13:29:47 +0530

    Updated website changes

--- src/app/api/create-order/route.ts
+++ src/app/api/create-order/route.ts
@@ -1,14 +1,14 @@
 import Razorpay from "razorpay";
 import { NextResponse } from "next/server";
 
+
+
+export async function POST(req: Request) {
+  const body = await req.json();
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID!,
   key_secret: process.env.RAZORPAY_KEY_SECRET!,
 });
-
-export async function POST(req: Request) {
-  const body = await req.json();
-
   const options = {
     amount: body.amount * 100,
     currency: "INR",
